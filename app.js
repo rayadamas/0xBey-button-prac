69cb05ed05c0c441a5d1c98c68be219f5404ed33
Ray Adamas <djadamasray@gmail.com>
2021-11-11 20:03:45 -0600

    `

---
+++ app.js
@@ -0,0 +1,105 @@
+// local reviews data
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "'m baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+
+// selecting items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randBtn = document.querySelector(".rand-btn");
+
+// set starting item
+let currItem = 0;
+
+// load init item on window obj
+window.addEventListener('DOMContentLoaded', function () {
+  showPerson(currItem);
+  // console.log('Big YERR vibes'); //fires when HTML doc has been fired + loaded up
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click', function () {
+  currItem++;
+  if(currItem > reviews.length - 1) {
+    currItem = 0;
+  }
+  showPerson(currItem);
+  console.log(currItem, img, author, job, info);
+});
+
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currItem--;
+  if(currItem < 0) {
+    currItem = reviews.length - 1;
+  }
+  showPerson(currItem);
+  console.log(currItem, img, author, job, info);
+});
+
+// show rand person
+randBtn.addEventListener('click', function () {
+  currItem = Math.floor(Math.random() * reviews.length); // choosing a rand person based on the amt we have to choose from
+  showPerson(currItem);
+  console.log(currItem, img, author, job, info);
+})
+
+
+
+
+
+
+
+
+
+
